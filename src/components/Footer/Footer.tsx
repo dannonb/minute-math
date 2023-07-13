2235924fef0887338ead165ef1fca9df2236fb39
dannon <53214621+dannonb@users.noreply.github.com>
2023-07-13 10:28:05 -0700

    added in additional links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,19 +4,10 @@ const Footer = () => {
   return (
     <footer className='fixed bottom-0 left-0 right-0 -z-10'>
         <div className='flex flex-col justify-between'>
-            <div className='w-full h-10 bg-[#A9BCD0] my-5'></div>
-            <div className='w-full h-4 bg-[#646F4B] my-5'></div>
-            <div className='w-full h-6 bg-[#373F51] my-5'></div>
-            <div className='w-full h-10 bg-[#D1603D] my-5'></div>
-            <div className='w-full h-4 bg-[#D4CB92] my-5'></div>
-            <div className='w-full h-10 bg-[#A9BCD0] my-5'></div>
-            <div className='w-full h-4 bg-[#646F4B] my-5'></div>
-            <div className='w-full h-6 bg-[#373F51] my-5'></div>
-            <div className='w-full h-10 bg-[#D1603D] my-5'></div>
-            <div className='w-full h-4 bg-[#D4CB92] my-5'></div>
-            <div className='w-full h-10 bg-[#A9BCD0] my-5'></div>
-            <div className='w-full h-4 bg-[#646F4B] my-5'></div>
-            <div className='w-full h-6 bg-[#373F51] my-5'></div>
+            <div className='w-full h-10 bg-[#A9BCD0]'></div>
+            <div className='w-full h-10 bg-[#373F51]'></div>
+            <div className='w-full h-10 bg-[#646F4B]'></div>
+            <div className='w-full h-10 bg-[#D4CB92]'></div>
         </div>
     </footer>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,63 +1,38 @@
-"use client"
-
-import React, { useState } from 'react'
-import { FaGithub, FaLinkedin, FaTwitter, FaBars, FaBarsStaggered } from 'react-icons/fa6'
-import { Menu, Transition } from '@headlessui/react'
+import React from 'react'
+import { 
+    SiGithub, 
+    SiLinkedin, 
+    SiTwitter,
+    SiFiverr
+} from 'react-icons/si'
+import { FaGlobe } from 'react-icons/fa'
 import Link from 'next/link'
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
-
     return (
         <header className='header border-b border-black'>
-            <div className='flex items-center justify-between mx-5 my-5 text-center'>
-                {/* <div className='w-1/3 flex justify-start'>
-                    <Menu>
-                        {({ open }) => (
-                            <>
-                                {
-                                    open ? (
-                                        <Menu.Button className='md:text-2xl text-xl'>
-                                            <FaBarsStaggered />
-                                        </Menu.Button>
-                                    ) : (
-                                        <Menu.Button className='md:text-2xl text-xl'>
-                                            <FaBars />
-                                        </Menu.Button>
-                                    )
-                                }
-                                <Transition
-                                    show={open}
-                                    enter="transition duration-100 ease-out"
-                                    enterFrom="transform scale-95 opacity-0"
-                                    enterTo="transform scale-100 opacity-100"
-                                    leave="transition duration-75 ease-out"
-                                    leaveFrom="transform scale-100 opacity-100"
-                                    leaveTo="transform scale-95 opacity-0"
-                                >
-                                    <Menu.Items static className='flex mx-5'>
-                                        <Menu.Item as='div' className='mx-5'>item1</Menu.Item>
-                                        <Menu.Item as='div'>item2</Menu.Item>
-                                    </Menu.Items>
-                                </Transition>
-                            </>
-                        )}
-                    </Menu>
-                </div> */}
+            <div className='flex items-center justify-between mx-5 pt-5 text-center'>
                 <div className='w-1/3'>
                     <h1 className='text-xl md:text-4xl font-bold'>Minute Math</h1>
                 </div>
                 <div className='flex justify-end w-1/3'>
-                    <Link href="https://github.com" className='md:text-3xl md:mx-5 text-lg mx-2'>
-                        <FaGithub />
+                    <Link href="https://github.com/dannonb/minute-math" target='_blank' className='md:text-3xl md:mx-5 text-lg mx-2'>
+                        <SiGithub />
                     </Link>
-                    <Link href="https://linkedin.com" className='md:text-3xl md:mx-5 text-lg mx-2'>
-                        <FaLinkedin />
+                    <Link href="https://linkedin.com/in/dannonbigham" target='_blank' className='md:text-3xl md:mx-5 text-lg mx-2'>
+                        <SiLinkedin />
                     </Link>
-                    <Link href="https://twitter.com" className='md:text-3xl md:mx-5 text-lg mx-2'>
-                        <FaTwitter />
+                    <Link href="https://twitter.com/devbydannon" target='_blank' className='md:text-3xl md:mx-5 text-lg mx-2'>
+                        <SiTwitter />
+                    </Link>
+                    <Link href="https://dannonbigham.com" target='_blank' className='md:text-3xl md:mx-5 text-lg mx-2'>
+                        <FaGlobe />
+                    </Link>
+                    <Link href="https://fiverr.com/devbydannon" target='_blank' className='md:text-3xl md:mx-5 text-lg mx-2'>
+                        <SiFiverr />
                     </Link>
                 </div>
+                
             </div>
         </header>
     )
