cdb4970e24f3d5ad3d5f12877ebd353dee855a65
dannon <53214621+dannonb@users.noreply.github.com>
2023-07-28 02:40:07 -0700

    setting time bg back to white

--- src/components/GameScreen/DisplayTime.tsx
+++ src/components/GameScreen/DisplayTime.tsx
@@ -6,7 +6,7 @@ interface IDisplayTimeProps {
 
 const DisplayTime = ({ time }: IDisplayTimeProps) => {
     return (
-        <div className={`flex mb-5 h-24 w-24 justify-center items-center border-2 border-black rounded-full ${time <= 10 && 'animate-pulse'}`}>
+        <div className={`flex mb-5 h-24 w-24 justify-center items-center border-2 border-black rounded-full bg-white ${time <= 10 && 'animate-pulse'}`}>
             <div className='text-center'>
                 <p className='text-3xl font-semibold'>{time}</p>
             </div>
